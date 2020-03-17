b7b1e51836b808af6979c735dd7aa7b426c78322
ddxx6317 <ddxx5317@163.com>
2020-03-17 17:21:21 +0800

    快速排序

--- src/main/java/com/ddxx/study/algorithms/demo/QuickSort.java
+++ src/main/java/com/ddxx/study/algorithms/demo/QuickSort.java
@@ -11,38 +11,30 @@ public class QuickSort {
         int arr[] = {1, 8, 5, 3, 2, 4};
         doQuickSort(arr,0,arr.length-1);
     }
-    public static void doQuickSort(int[] array, int left, int right) {
-        if(left > right) {
+    public static int partition(int[] array,int low,int high){
+        int base = array[low];
+        while(low < high){
+            while(array[high] >= base && high > low){
+                high--;
+            }
+            array[low] = array[high];
+            while(array[low] <= base && high > low){
+                low++;
+            }
+            array[high] = array[low];
+        }
+
+        // 此时low=high、所以array[low] = array[low] = base，即返回low和high结果一样;
+        array[low] = base;
+        return low;
+    }
+
+    public static void doQuickSort(int[] array,int low ,int high){
+        if(low >= high){
             return;
         }
-        // base中存放基准数
-        int base = array[left];
-        int i = left, j = right;
-        while(i < j) {
-            // 顺序很重要，先从右边开始往左找，直到找到比base值小的数
-            while(array[j] >= base && i < j) {
-                j--;
-            }
-            // 再从左往右边找，直到找到比base值大的数
-            while(array[i] <= base && i < j) {
-                i++;
-            }
-
-            // 上面的循环结束表示找到了位置或者(i>=j)了，交换两个数在数组中的位置
-            if(i < j) {
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-            }
-        }
-
-        //将基准数放到中间的位置（基准数归位）
-        array[left] = array[i];
-        array[i] = base;
-
-        // 递归，继续向基准的左右两边执行和上面同样的操作
-        // i的索引处为上面已确定好的基准值的位置，无需再处理
-        doQuickSort(array, left, i - 1);
-        doQuickSort(array, i + 1, right);
+        int index = partition(array,low,high);
+        doQuickSort(array,low,index-1);
+        doQuickSort(array,index+1,high);
     }
 }
