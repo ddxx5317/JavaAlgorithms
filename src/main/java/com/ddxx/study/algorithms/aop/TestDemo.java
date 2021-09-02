4d390a5a3eca28190d80adb9344e9cb9e8ff009f
ddxx6317 <ddxx5317@163.com>
2021-09-02 21:23:00 +0800

    jol

---
+++ src/main/java/com/ddxx/study/algorithms/aop/TestDemo.java
@@ -0,0 +1,39 @@
+package com.ddxx.study.algorithms.aop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @Description:
+ * @Author: DDxx
+ * @Date: 2020/11/20
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        //Long base = 0x004000000L;
+        Long base2 = 0b100000000000000000000000000L;
+        System.out.println(base2);
+        Long base = 0x004000000L;
+        System.out.println(Long.toBinaryString(base));
+        System.out.println(base);
+
+        List<Long> relatedOrderIds1 = new ArrayList<>();
+        relatedOrderIds1.add(28324382L);
+        relatedOrderIds1.add(28324482L);
+
+        buildParentOrderId(base,relatedOrderIds1);
+
+        List<Long> relatedOrderIds2 = new ArrayList<>();
+        relatedOrderIds2.add(28324382L);
+        relatedOrderIds2.add(28324482L);
+        buildParentOrderId(base,relatedOrderIds2);
+    }
+    private static void buildParentOrderId(final Long base,final List<Long> relatedOrderIds) {
+        AtomicReference<Long> parentOrderId = new AtomicReference(base);
+        relatedOrderIds.forEach(relatedOrderId ->{
+            parentOrderId.updateAndGet(result -> result | relatedOrderId);
+        });
+        System.out.println(parentOrderId.get());
+    }
+}
